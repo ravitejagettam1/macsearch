3dcd98f359a9f57ffc0844c69c9af729570a25cd
ravitejagettam1 <37352111+ravitejagettam1@users.noreply.github.com>
2019-06-13 21:24:01 -0700

    Update Mac_search.py

--- Mac_search.py
+++ Mac_search.py
@@ -3,14 +3,16 @@ import argparse
 
 class Mac_address:
     def __init__(self,mac_address):
-        self.api_key=self.get_api_key()+"{}".format("wACx_ta")
-        self.api_key=self.api_key[::-1]
-        self.address="api.macaddress.io"
-        self.api_version="v1"
-        self.key1="apiKey"
-        self.url = "https://{}/{}?{}={}&output=json&search={}".format(self.address,self.api_version,self.key1,self.api_key,mac_address)
-        self.get_mac_address()
-
+        try:
+            self.api_key=self.get_api_key()+"{}".format("wACx_ta")
+            self.api_key=self.api_key[::-1]
+            self.address="api.macaddress.io"
+            self.api_version="v1"
+            self.key1="apiKey"
+            self.url = "https://{}/{}?{}={}&output=json&search={}".format(self.address,self.api_version,self.key1,self.api_key,mac_address)
+            self.get_mac_address()
+        Except:
+            print("Unable to connect to internet..")
     def get_api_key(self):
         key=self.decrypt("6jGBLJdqTxPz3612NUhBQa")
         return key
